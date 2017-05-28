440fce8db86633cdb89efa4f3945e987fa036d94
AgtLucas <lucas@agtlucas.com>
2017-05-28 10:59:47 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,82 @@
-import React from 'react'
-import { InputWithLabel as Input } from './components/Input'
-import { H1, Button, Form, Wrapper } from './components/Misc'
+// @flow
+import React, { Component } from 'react'
+import { H1, Wrapper } from './components/Misc'
+import FormContainer from './containers/FormContainer'
+import ResultContainer from './containers/ResultContainer'
 import { round, trunc } from './utils'
 
-const App = props => (
-  <Wrapper width='30em'>
-    <H1>"Pfand" calculator</H1>
-    <Form onSubmit={props.onSubmit}>
-      <Input label='Water price' value={props.waterPrice.toString()} onChange={props.onChangeWaterPrice} />
-      <Input label='I have: ' value={props.money.toString()} onChange={props.onUpdateMoney} />
-      <Button disabled={props.money === 0}>Calculate</Button>
-    </Form>
-  </Wrapper>
-)
+class App extends Component {
+  state = {
+    waterPrice: 0.70,
+    tarePrice: 0.25,
+    money: 0,
+    bottlesForMoney: 0,
+    cashRemaining: 0,
+    cashForTare: 0,
+    total: 0
+  }
+
+  onChangeWaterPrice = (e: any) => {
+    this.setState({ waterPrice: e.currentTarget.value })
+  }
+
+  onChangeMoney = (e: any) => {
+    this.setState({ money: e.currentTarget.value })
+  }
+
+  bottlesForMoney (money: number) {
+    return trunc(money / this.state.waterPrice)
+  }
+
+  cashRemaining (numberOfBottles: number) {
+    let cashRemaining = this.state.money - this.state.money
+    return round(cashRemaining)
+  }
+
+  moneyForTare (numberOfBottles: number) {
+    let moneyForTare = numberOfBottles * this.state.tarePrice
+    return round(moneyForTare)
+  }
+
+  calculate () {
+    let bottles = this.bottlesForMoney(this.state.money)
+    let cashRemaining = this.cashRemaining(bottles)
+    let cashForTare = this.moneyForTare(bottles)
+    let cash = cashRemaining + cashForTare
+    let total = round(cash)
+    this.setState({
+      bottlesForMoney: bottles,
+      cashRemaining,
+      cashForTare,
+      total
+    })
+  }
+
+  handleSubmit = (e: any) => {
+    e.preventDefault()
+    this.calculate()
+  }
+
+  render () {
+    return (
+      <Wrapper width='30em'>
+        <H1>"Pfand" calculator</H1>
+        <FormContainer
+          onSubmit={this.handleSubmit}
+          waterPrice={this.state.waterPrice}
+          onChangeWaterPrice={this.onChangeWaterPrice}
+          money={this.state.money}
+          onChangeMoney={this.onChangeMoney}
+        />
+        <ResultContainer
+          bottlesForMoney={this.state.bottlesForMoney}
+          cashRemaining={this.state.cashRemaining}
+          cashForTare={this.state.cashForTare}
+          total={this.state.total}
+        />
+      </Wrapper>
+    )
+  }
+}
 
 export default App
